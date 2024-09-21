1126997ddfd941e30ac6a56adeb3d4789bd7fbaa
AJAY VENKATESH BABU <ajayvengateshbabu@gmail.com>
2024-09-21 12:43:11 +0300

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function() {
+
+    const element = document.getElementById("level");
+
+   
+    let level = 0;
+
+    let currentElement = element;
+    while (currentElement) {
+        level++;
+        currentElement = currentElement.parentElement; // Move to the parent element
+    }
+
+   
+    alert(`The level of the element is: ${level}`);
+});
